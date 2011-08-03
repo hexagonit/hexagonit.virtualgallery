1850de1d4e275ca48645e88f3c8bb82b6263575d
Nejc Zupan <nejc.zupan@gmail.com>
2011-08-03 15:56:04 +0300

    merge interfaces.py and browser/interfaces.py

--- hexagonit/virtualgallery/browser/interfaces.py
+++ hexagonit/virtualgallery/browser/interfaces.py
@@ -3,3 +3,9 @@ from zope.interface import Interface
 
 class IHexagonitVirtualgalleryLayer(Interface):
     """Marker interface for browserlayer."""
+
+
+class IVirtualgalleryEnabled(Interface):
+    """Add this marker interface to your content-type to enable display of
+    virtual gallery.
+    """
--- hexagonit/virtualgallery/interfaces.py
+++
@@ -1,7 +0,0 @@
-from zope.interface import Interface
-
-
-class IVirtualgalleryEnabled(Interface):
-    """Add this marker interface to your content-type to enable display of
-    virtual gallery.
-    """
