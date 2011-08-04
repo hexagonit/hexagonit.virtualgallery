03e741e027b10b403261441fbdfe6aabe128555e
Nejc Zupan <nejc.zupan@gmail.com>
2011-08-04 13:24:13 +0300

    improved code readability in data.py

--- hexagonit/virtualgallery/browser/data.py
+++ hexagonit/virtualgallery/browser/data.py
@@ -8,56 +8,61 @@ from Products.Five.browser import BrowserView
 import json
 
 
-def jsonize(items):
-    images = []
-    for item in items:
-        images.append(dict(
-            url=item.getURL(),
-            description=item.Description(),
-            author=item.Creator(),
-            title=item.Title(),
-        ))
-    return json.dumps({
-        "ui": dict(
-            next="next",
-            prev="prev",
-            forward="forward",
-            backward="backward",
-            left="left",
-            right="right",
-            anaglyph="anaglyph",
-            fullscreen="fullscreen",
-            loadingImg="loading image:",
-            spaceToEnterRoom="press space to enter",
-            spaceToCloseUp="press space to zoom in",
-            enterRoom="You entering room no [x] of [y]",
-            enterRoomToolTip="Enter [x] room",
-            roomName="Room [x]",
-        ),
-        "settings": dict(
-            anaglyphModeEnabled="true",
-        ),
-        "images": images,
-    })
+class JSONBase(BrowserView):
+    """Base class for generating JSON data for virtual gallery."""
 
-
-class FolderJSON(BrowserView):
-    """A BrowserView to generate Virtual Gallery JSON data for folders."""
-
-    def __call__(self):
-        # get contents of this Folder
-        items = self.context.restrictedTraverse('@@folderListing')()
+    def jsonize(self, items):
 
         # filter out non-image objects
         image_types = getToolByName(self.context, 'portal_tinymce').imageobjects
         items = [item for item in items if item.Type() in image_types]
 
-        # response
+        # construct list of images
+        images = []
+        for item in items:
+            images.append(dict(
+                url=item.getURL(),
+                description=item.Description(),
+                author=item.Creator(),
+                title=item.Title(),
+            ))
+
+        # set response and return
         self.request.RESPONSE.setHeader('Content-Type', 'application/json')
-        return jsonize(items)
+        return json.dumps({
+            "ui": dict(
+                next="next",
+                prev="prev",
+                forward="forward",
+                backward="backward",
+                left="left",
+                right="right",
+                anaglyph="anaglyph",
+                fullscreen="fullscreen",
+                loadingImg="loading image:",
+                spaceToEnterRoom="press space to enter",
+                spaceToCloseUp="press space to zoom in",
+                enterRoom="You entering room no [x] of [y]",
+                enterRoomToolTip="Enter [x] room",
+                roomName="Room [x]",
+            ),
+            "settings": dict(
+                anaglyphModeEnabled="true",
+            ),
+            "images": images,
+        })
 
 
-class TopicJSON(BrowserView):
+class FolderJSON(JSONBase):
+    """A BrowserView to generate Virtual Gallery JSON data for folders."""
+
+    def __call__(self):
+        # get contents of this Folder
+        items = self.context.restrictedTraverse('@@folderListing')()
+        return self.jsonize(items)
+
+
+class TopicJSON(JSONBase):
     """A BrowserView to generate Virtual Gallery JSON data for collections."""
 
     def __call__(self):
@@ -66,13 +71,6 @@ class TopicJSON(BrowserView):
         query = self.context.buildQuery()
         brains = catalog(query)
 
-        # convert catalog brains into plone.app.contentlisting items
-        items = IContentListing(brains)
+        # convert catalog brains into plone.app.contentlisting items and return
+        return self.jsonize(IContentListing(brains))
 
-        # filter out non-image objects
-        image_types = getToolByName(self.context, 'portal_tinymce').imageobjects
-        items = [item for item in items if item.Type() in image_types]
-
-        # response
-        self.request.RESPONSE.setHeader('Content-Type', 'application/json')
-        return jsonize(items)
