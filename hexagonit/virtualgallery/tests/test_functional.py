22ac038e657909b9bc4f2c0b35bbaecaa024858b
Nejc Zupan <nejc.zupan@gmail.com>
2011-08-04 10:17:29 +0300

    fixed path to functional test file

--- hexagonit/virtualgallery/tests/test_functional.py
+++ hexagonit/virtualgallery/tests/test_functional.py
@@ -74,5 +74,5 @@ def DocFileSuite(testfile, flags=FLAGS, setUp=setUp, layer=FUNCTIONAL_TESTING):
 
 def test_suite():
     return unittest.TestSuite([
-        DocFileSuite('functional/browser.txt'),
+        DocFileSuite('functional/story.txt'),
         ])
