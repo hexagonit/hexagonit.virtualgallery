3dfbccebe67bd620b923e71d8a64341648fc247e
Kai Lautaportti <kai.lautaportti@hexagonit.fi>
2011-08-18 18:06:47 +0300

    Fixed test regressions.

--- hexagonit/virtualgallery/schema.py
+++ hexagonit/virtualgallery/schema.py
@@ -3,7 +3,7 @@ GALLERY_DATA_SCHEMA = {
     "name": "Data source for Virtual 3D gallery",
     "type": "object",
     "properties": {
-        "ui": {
+        "images": {
             "name": "Images to display in the Virtual Gallery",
             "type": "array",
             "items": {
@@ -44,30 +44,6 @@ GALLERY_DATA_SCHEMA = {
             "name": "Virtual Gallery UI strings",
             "type": "object",
             "properties": {
-                "next": {
-                    "type": "string",
-                    "required": True,
-                },
-                "prev": {
-                    "type": "string",
-                    "required": True,
-                },
-                "forward": {
-                    "type": "string",
-                    "required": True,
-                },
-                "backward": {
-                    "type": "string",
-                    "required": True,
-                },
-                "left": {
-                    "type": "string",
-                    "required": True,
-                },
-                "right": {
-                    "type": "string",
-                    "required": True,
-                },
                 "anaglyph": {
                     "type": "string",
                     "required": True,
@@ -80,23 +56,11 @@ GALLERY_DATA_SCHEMA = {
                     "type": "string",
                     "required": True,
                 },
-                "spaceToEnterRoom": {
-                    "type": "string",
-                    "required": True,
-                },
-                "spaceToCloseUp": {
-                    "type": "string",
-                    "required": True,
-                },
                 "enterRoom": {
                     "type": "string",
                     "required": True,
                 },
                 "enterRoomToolTip": {
-                    "type": "string",
-                    "required": True,
-                },
-                "roomName": {
                     "type": "string",
                     "required": True,
                 },
